0f2e7565e430557209b0e3e74d8cf7ff0eacee85
Vexatos <Vexatos@users.noreply.github.com>
2015-03-22 13:07:15 +0100

    Forgot the import

--- src/main/java/crazypants/enderio/teleport/telepad/PacketUpdateCoords.java
+++ src/main/java/crazypants/enderio/teleport/telepad/PacketUpdateCoords.java
@@ -4,6 +4,7 @@ import io.netty.buffer.ByteBuf;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import crazypants.enderio.EnderIO;
 import crazypants.enderio.network.MessageTileEntity;
 import crazypants.util.BlockCoord;
 
